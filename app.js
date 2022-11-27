51a481e48bf985a6cfac930fa4b066380d09ce28
Shantanu Singh <shannformal323@gmail.com>
2022-11-27 12:55:58 +0530

    added heroku Procfile and port for heroku

--- app.js
+++ app.js
@@ -176,4 +176,4 @@ app.post('/update', (req, res) => {
 })
 
 // server running on port 3000
-app.listen(3000, () => console.log('Server is running on port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('Server is running on port 3000'));
